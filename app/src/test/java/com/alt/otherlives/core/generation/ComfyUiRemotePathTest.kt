4c4f095d95d25cc7aca936f9b1fa6d39fd36ab21
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-24 18:30:56 +0200

    test: cover ComfyUI remote path validation

---
+++ app/src/test/java/com/alt/otherlives/core/generation/ComfyUiRemotePathTest.kt
@@ -0,0 +1,38 @@
+package com.alt.otherlives.core.generation
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ComfyUiRemotePathTest {
+    @Test
+    fun acceptsCanonicalFilenameAndSubfolder() {
+        assertEquals(
+            "scene.png",
+            ComfyUiRemotePath.validateFilename("scene.png")
+        )
+        assertEquals(
+            "outputs/alt",
+            ComfyUiRemotePath.validateSubfolder("outputs/alt")
+        )
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun rejectsFilenameTraversal() {
+        ComfyUiRemotePath.validateFilename("../scene.png")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun rejectsSubfolderTraversal() {
+        ComfyUiRemotePath.validateSubfolder("outputs/../private")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun rejectsAbsoluteSubfolder() {
+        ComfyUiRemotePath.validateSubfolder("/tmp")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun rejectsOversizedFilename() {
+        ComfyUiRemotePath.validateFilename("a".repeat(256))
+    }
+}
