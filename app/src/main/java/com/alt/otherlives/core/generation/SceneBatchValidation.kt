27a631c8ca233a75457bdfb7f54fd6deec4b0814
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-26 13:45:18 +0200

    refactor: use shared timeline chapter limit in storage

--- app/src/main/java/com/alt/otherlives/core/generation/SceneBatchValidation.kt
+++ app/src/main/java/com/alt/otherlives/core/generation/SceneBatchValidation.kt
@@ -1,15 +1,16 @@
 package com.alt.otherlives.core.generation
 
+import com.alt.otherlives.core.model.TimelineConstraints
+
 internal object SceneBatchValidation {
-    const val MAX_CHAPTER_COUNT = 5
 
     fun isSupportedChapterIndex(index: Int): Boolean =
-        index in 0 until MAX_CHAPTER_COUNT
+        index in 0 until TimelineConstraints.MAX_CHAPTER_COUNT
 
     fun validateChapterIndexes(indexes: List<Int>) {
         require(indexes.all(::isSupportedChapterIndex)) {
             "Generated scene chapter indexes must be between 0 and " +
-                (MAX_CHAPTER_COUNT - 1)
+                (TimelineConstraints.MAX_CHAPTER_COUNT - 1)
         }
         require(indexes.distinct().size == indexes.size) {
             "Generated scene batch contains duplicate chapter indexes"
